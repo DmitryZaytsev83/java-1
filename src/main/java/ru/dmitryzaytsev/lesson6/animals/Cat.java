c3f6fccbd539f3a160c115c3fe05af3726076f01
Dmitry <dmitry.zaytsev83@gmail.com>
2021-01-29 14:23:13 +0300

    finished homework 6

---
+++ src/main/java/ru/dmitryzaytsev/lesson6/animals/Cat.java
@@ -0,0 +1,21 @@
+package ru.dmitryzaytsev.lesson6.animals;
+
+public class Cat extends Animal {
+    static int count = 0;
+
+    public Cat(String name) {
+        super(name);
+        this.maxRunLength = 200;
+        this.maxSwimLength = 0;
+        count++;
+    }
+
+    @Override
+    public void swim(int length) {
+        System.out.printf("%s не поплыл, потому что забил%n", name);
+    }
+
+    public static void getCount() {
+        System.out.printf("Количество котов: %d%n", count);
+    }
+}
---
+++ src/main/java/ru/dmitryzaytsev/lesson6/animals/Dog.java
@@ -0,0 +1,25 @@
+package ru.dmitryzaytsev.lesson6.animals;
+
+public class Dog extends Animal {
+    static int count = 0;
+
+    public Dog(String name) {
+        super(name);
+        this.maxRunLength = 500;
+        this.maxSwimLength = 10;
+        count++;
+    }
+
+    @Override
+    public void swim(int length) {
+        if (length <= maxSwimLength) {
+            System.out.printf("%s проплыл %d метров%n", name, length);
+        } else {
+            System.out.printf("%s проплыл %d метров и утонул, не доплыв %d метров%n", name, maxSwimLength, length - maxSwimLength);
+        }
+    }
+
+    public static void getCount() {
+        System.out.printf("Количество собак: %d%n", count);
+    }
+}
